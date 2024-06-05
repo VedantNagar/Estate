e56f8b1fdfa6904c3a90108ce86ad3a248fe38f5
Vedant <vedantnagar21@gmail.com>
2024-06-06 00:30:55 +0530

    connected database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
-const express = require("express");
+import express from "express";
+import connectDB from "./database/connectDB.js";
+import { configDotenv } from "dotenv";
+configDotenv();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
