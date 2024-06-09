eae4d9cabbcf56535f997bf9df27e8865d073841
Vedant <vedantnagar21@gmail.com>
2024-06-09 20:03:41 +0530

    chore: Add user route to backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import connectDB from "./database/connectDB.js";
 import { configDotenv } from "dotenv";
+import userRouter from "./routes/userRoute.js";
 configDotenv();
 
 const app = express();
+
+app.use(express.json());
+app.use("/api/user", userRouter);
 
 const port = process.env.PORT || 3000;
 const start = async () => {
