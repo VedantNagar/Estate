5a89f9b70321d79706fac1e89fd81f2d4a0a0c64
Vedant <vedantnagar21@gmail.com>
2024-06-14 22:48:43 +0530

    chore: Started signin functionality to authController.js with error handling

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -12,3 +12,19 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      errorHandler("User not found", 404);
+    }
+    const correctPassword = await bcrypt.compare(password, user.password);
+    if (!correctPassword) {
+      errorHandler("Invalid password", 401);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
