0560351d49a89ce4229c36bf42b0e414b2dca6a9
Vedant <vedantnagar21@gmail.com>
2024-06-16 14:05:33 +0530

    chore: Add signin functionality to frontend and backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="signin" element={<SignIn />} />
           <Route path="/profile" element={<Profile />} />
--- frontend/src/Pages/SignIn.jsx
+++ frontend/src/Pages/SignIn.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const SignIn = () => {
-  return <div>SignIn</div>;
+const signin = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/signin",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.data.success === false) {
+        setLoading(false);
+        setError(response.data.message);
+        return;
+      }
+      console.log(response.data);
+      setLoading(false);
+      setError(null);
+      navigate("/home");
+    } catch (error) {
+      setLoading(false);
+      console.error("Error:", error.response?.data || error.message);
+      setError(error.response?.data.message || "An error occurred");
+    }
+  };
+  return (
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center">
+      <div className="text-3xl text-center pt-16 font-semibold">
+        <h1>Sign In</h1>
+      </div>
+      <form className="mt-10 max-w-lg w-full px-6" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="E-mail"
+          value={data.email}
+          id="email"
+          className="border mt-4 p-2 rounded-lg w-full"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={data.password}
+          id="password"
+          className="border mt-4 p-2 rounded-lg w-full"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          type="submit"
+          className="bg-blue-700 hover:bg-blue-500 text-white font-semibold py-2 px-4 mt-4 rounded-lg w-full"
+        >
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+      </form>
+      <div className="flex mt-5 text-left gap-1">
+        <p>Don't have an account?</p>
+        <Link to={"/signup"}>
+          <span className="text-blue-700">Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-4">{error}</p>}
+    </div>
+  );
 };
 
-export default SignIn;
+export default signin;
